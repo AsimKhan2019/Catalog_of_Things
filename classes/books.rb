6df5f165a9063630d8a983bde86a939be906044a
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-10-11 14:33:38 +0500

    Changed parameters to intialize

--- classes/books.rb
+++ classes/books.rb
@@ -3,7 +3,7 @@ require_relative 'items'
 class Book < Item
   attr_accessor :publisher :cover_state
 
-  def initialize(publisher = 'Unknown', cover_state = 'Unknown', publish_date)
+  def initialize(publisher = 'Unknown', publish_date, cover_state = 'Unknown')
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
