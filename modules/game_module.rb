d6a538e49129ba2982ded912ec4c9130158c4882
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-10-12 12:40:27 +0100

    add pass all parameters from game details to the add game method

--- modules/game_module.rb
+++ modules/game_module.rb
@@ -4,5 +4,12 @@ module GameModule
         gets.chomp
     end
     def add_new_game_details
+        puts "\n-------------------------------"
+        puts "\nENTER GAME DETAILS\n\n"
+
+        game_name = capture_input('Input Game Name: ')
+        last_played_at = capture_input('Last Play Date: ')
+        multiplayer = capture_input('Multiplayer Game Name: ')
+        add_game([game_name, last_played_at, multiplayer])
     end
 end
